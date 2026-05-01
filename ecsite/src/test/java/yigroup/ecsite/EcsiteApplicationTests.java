d16d5aba18aec2bfa9303b332dec75e0729a6915
yigroup-kyouiku <bbkyouiku@gmail.com>
2026-05-01 16:28:36 +0900

    add project file

---
+++ ecsite/src/test/java/yigroup/ecsite/EcsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package yigroup.ecsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
